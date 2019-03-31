0582d7f9f285463e7152a411d64e6c703dd3c1df
DevJeeth <konerirajb@yahoo.com>
2019-03-31 18:22:04 +0530

    Imported Unka Asset Imported Easy Touch Working on dragon controls for mobile input

---
+++ Assets/0_DragonPrince/1_Scripts/PlayerControllerManager.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using HedgehogTeam.EasyTouch;
+using MalbersAnimations;
+
+public class PlayerControllerManager : MonoBehaviour
+{
+	private Animal m_refDragonInput;
+	private MFreeLookCamera m_refFreeLookCamera;
+
+	private float m_fHorizontal, m_fVertical, m_fLookHorizontal, m_fLookVertical;
+
+    void Start()
+    {
+		m_refDragonInput = gameObject.GetComponent<Animal>();
+		m_refFreeLookCamera = GameObject.FindGameObjectWithTag("MainCamera").gameObject.GetComponent<MFreeLookCamera>();
+
+		if(m_refFreeLookCamera == null)
+		{
+			Debug.LogError("Camera not found");
+		}
+
+	}
+
+   
+    void Update()
+    {
+
+		if(m_refFreeLookCamera == null)
+				return;
+
+		m_fHorizontal = ETCInput.GetAxis("Horizontal");
+		m_fVertical  = ETCInput.GetAxis("Vertical");
+
+		m_fLookHorizontal = ETCInput.GetAxis("LookHorizontal");
+		m_fLookVertical = ETCInput.GetAxis("LookVertical");
+
+		m_refDragonInput.MovementAxis = new Vector2(m_fHorizontal,m_fVertical);
+		m_refDragonInput.MovementForward = m_fVertical;
+    }
+}
