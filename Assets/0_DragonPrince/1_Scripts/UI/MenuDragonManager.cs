fdd4afa14787d0f7398851af4b4d8a4d0f2ac582
DevJeeth <konerirajb@yahoo.com>
2019-10-08 20:43:10 +0530

    +Menu Dragon particle effect and sound

--- Assets/0_DragonPrince/1_Scripts/UI/MenuDragonManager.cs
+++ Assets/0_DragonPrince/1_Scripts/UI/MenuDragonManager.cs
@@ -7,16 +7,30 @@ namespace DragonLore
 {
 	public class MenuDragonManager : MonoBehaviour
 	{
+		public ParticleSystem m_particleDust;
+		public AudioClip m_audioWing;
+		private AudioSource m_audioSource;
 
 		void Start()
 		{
-
+			m_audioSource = GetComponent<AudioSource>();
+			m_audioSource.clip = m_audioWing;
 		}
 
 
 		void Update()
 		{
 
+		}
+
+		public void WingsSoundEffect()
+		{
+			m_audioSource.Play();
+		}
+
+		public void WingsCloudEffect()
+		{
+			m_particleDust.Play();
 		}
 	}
 }
