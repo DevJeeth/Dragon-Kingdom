a4039654c61a1aa0602be08573c895c89eaf0340
DevJeeth <konerirajb@yahoo.com>
2019-10-08 20:18:09 +0530

    +Assets imported

---
+++ Assets/0_DragonPrince/1_Scripts/UI/MenuDragonManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using WeplayVR.Base;
+
+namespace DragonLore
+{
+	public class MenuDragonManager : MonoBehaviour
+	{
+
+		void Start()
+		{
+
+		}
+
+
+		void Update()
+		{
+
+		}
+	}
+}
