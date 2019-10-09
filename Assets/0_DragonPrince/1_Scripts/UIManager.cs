dde389b720f7bee4bc1a43312464a3d5f8221d84
DevJeeth <konerirajb@yahoo.com>
2019-10-09 08:45:22 +0530

    +Menu Simple Tween

--- Assets/0_DragonPrince/1_Scripts/UIManager.cs
+++ Assets/0_DragonPrince/1_Scripts/UIManager.cs
@@ -2,40 +2,70 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using DG.Tweening;
 
 public class UIManager : MonoBehaviour
 {
 	[SerializeField]
-	private Text m_textLogToUI;
-	private GameManager m_refGameManager;
+	private RectTransform m_rectGameTitle;
+	[SerializeField]
+	private RectTransform[] m_arrMenuButtons;
 
 	private void Awake()
 	{
-		Application.logMessageReceived += GameLogs;
+		DOTween.Init(true, true, LogBehaviour.Default);
+
+		RectTransform rectChild;
+
+		//Moving all UI off the screen
+		foreach(Transform child in transform)
+		{
+			rectChild = child.GetComponent<RectTransform>();
+			if (rectChild == null)
+				continue;
+
+			rectChild.anchoredPosition = new Vector2(rectChild.anchoredPosition.x - 500, rectChild.anchoredPosition.y);
+		}
 	}
 
 	// Start is called before the first frame update
 	private void Start()
     {
-		m_refGameManager = FindObjectOfType<GameManager>();
-		if(m_refGameManager == null)
+		GameTitleTweenStart();
+	}
+
+
+
+	private void GameTitleTweenStart()
+	{
+		m_rectGameTitle.DOAnchorPos(new Vector2(13, -95), 1).OnComplete(GameTitleTweenComplete);
+	}
+
+	private void GameTitleTweenComplete()
+	{
+		Debug.Log("[UIManager] Game Title Tween Complete");
+		ButtonTweenStart();
+	}
+
+	private void ButtonTweenStart()
+	{
+		Debug.Log("[UIManager] Button Tween Started");
+		m_iCount = 0; 
+		for (int i = 0; i<m_arrMenuButtons.Length; i++)
 		{
-			Debug.LogError("[UIManager] GameManager reference not found");
+			m_arrMenuButtons[i].DOAnchorPos(new Vector2(73, m_arrMenuButtons[i].anchoredPosition.y), 1).SetDelay(i*0.25f).OnComplete(ButtonTweenComplete);
 		}
-		
 	}
 
-	private void GameLogs(string logString, string stackTrace, LogType type)
+	private int m_iCount = 0;
+	private void ButtonTweenComplete()
 	{
-		if (m_textLogToUI == null)
-			return;
+		m_iCount++;
 
-		m_textLogToUI.text += "\n" + type.ToString() + logString;
-	}
-
-	public void StartServerConnection()
-	{
-		WarpNetworkManager.Instance.ConnectedToServer();
+		if(m_iCount >= m_arrMenuButtons.Length)
+		{
+			Debug.Log("[UIManager] Button Tween Complete");
+		}
 	}
 
 
