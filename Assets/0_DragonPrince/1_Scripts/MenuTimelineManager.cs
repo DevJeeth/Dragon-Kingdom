ac4f52571decb45a2a656ec2eeead49ea9f31a24
DevJeeth <konerirajb@yahoo.com>
2019-10-10 00:31:30 +0530

    Working on +Timeline +Cinemachine +UI tween

---
+++ Assets/0_DragonPrince/1_Scripts/MenuTimelineManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Playables;
+using UnityEngine.Timeline;
+
+public class MenuTimelineManager : MonoBehaviour
+{
+
+	public TimelineAsset m_timeline;
+	private Playable m_director;
+	private Dictionary<string, double> m_dicMarkers = new Dictionary<string, double>();
+	private Marker m_marker;
+    void Start()
+    {
+		m_director = GetComponent<Playable>();
+
+	}
+
+
+    void Update()
+    {
+        
+    }
+}
